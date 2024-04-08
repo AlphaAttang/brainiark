cfcd30f6a43494e13dea7a8a99a9cb697b7062a1
AlphaAttang <alphaa.attang@gmail.com>
2024-04-08 23:06:37 +0100

    testimonials section

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import './Testimonials.css'
+import testimonies from '../Assets/testimonies'
+import Testimonies from '../Testimonies/Testimonies'
 
 const Testimonials = () => {
   return (
     <div className='testimonials'>
-        <div className="testimonials-head">
-            <h1>What our <span>clients</span> are saying</h1>
-            <p>Prioritize what is important like some of our other owners.</p>
+      <div className="testimonials-head">
+        <h1>What our <span>clients</span> are saying</h1>
+        <p>Prioritize what is important like some of our other owners.</p>
+      </div>
+      <div className="testimonials-card">
+        <div className="testimonials-slide">
+            {testimonies.map((testimony, i) => {
+              return <Testimonies
+                key={i}
+                id={testimony.id}
+                image={testimony.image}
+                testimony={testimony.testimony}
+                name={testimony.name}
+                profession={testimony.profession}
+              />
+            })}
         </div>
-        <div className="testimonials-card">
-            
-        </div>
+      </div>
     </div>
   )
 }
